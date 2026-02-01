44c3b03a55e08ecef40a1ea5609496dd6cb3b03d
teusto <pteutoscano@gmail.com>
2026-02-01 12:15:52 +0000

    fix: update footer link

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./styles/footer.module.scss";
 const Footer = () => {
     return (
         <div className={styles.footer_wrapper}>
-            <p>2025 <span>Domenica.cria</span> — May this site be a balm upon you.</p>
+            <p>2025 <span><a href="https://domenica.agency" target="_blank" rel="noopener noreferrer">Domenica.agency</a></span> — May this site be a balm upon you.</p>
             <div className={styles.footer_links}>
                 <Link to="/privacy" className={styles.link}>Privacy Policy</Link>
                 <Link to="/impressum" className={styles.link}>Impressum</Link>
