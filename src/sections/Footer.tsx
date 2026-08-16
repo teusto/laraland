c4dad7e2792d3360ef2d2f6299b7c64fd6290db8
teusto <pteutoscano@gmail.com>
2026-08-16 13:18:30 +0100

    remove link to impressum

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -9,7 +9,8 @@ const Footer = () => {
             <p>2025 <span><a href="https://domenica.agency" target="_blank" rel="noopener noreferrer">Domenica.agency</a></span> — {t("footer.credit").split("<a>").pop()?.split("</a>").pop() || "May this site be a balm upon you."}</p>
             <div className={styles.footer_links}>
                 <Link to="/privacy" className={styles.link}>{t("footer.privacyLink")}</Link>
-                <Link to="/impressum" className={styles.link}>{t("footer.impressumLink")}</Link>
+                {/* <Link to="/impressum" className={styles.link}>{t("footer.impressumLink")}</Link> */}
+                <Link to="/" className={styles.link} disabled>(under construction)</Link>
             </div>
         </div>
     )
